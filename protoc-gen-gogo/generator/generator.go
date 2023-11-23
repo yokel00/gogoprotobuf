08e229fa70845f2de09b1ebda8ea4bd94a28dce8
liaolin <yokel0@foxmail.com>
2023-11-23 11:05:04 +0800

    readme

--- protoc-gen-gogo/generator/generator.go
+++ protoc-gen-gogo/generator/generator.go
@@ -1058,7 +1058,7 @@ func (g *Generator) printAtom(v interface{}) {
 		fmt.Fprint(g, v)
 	case *bool:
 		fmt.Fprint(g, *v)
-	case int:
+	case int, int32, uint32, int64, uint64:
 		fmt.Fprint(g, v)
 	case *int32:
 		fmt.Fprint(g, *v)
@@ -2656,7 +2656,11 @@ func (g *Generator) generateSetters(mc *msgCtx, topLevelFields []topLevelField) 
 // generateCommonMethods adds methods to the message that are not on a per field basis.
 func (g *Generator) generateCommonMethods(mc *msgCtx) {
 	// Reset, String and ProtoMessage methods.
+	g.P("// 返回类型名,为效率代替reflect.TypeOf().Name()")
+	g.P("func (m *", mc.goName, ") MsgName() string { return \"", mc.goName, "\" }")
+	g.P("// 返回hash64 对MsgName的xxhash.Sum64")
 	g.P("func (m *", mc.goName, ") MsgId64() uint64 { return ", xxhash.Sum64([]byte(mc.goName)), " }")
+	g.P("// 返回hash32 对MsgName的murmur3.Sum32")
 	g.P("func (m *", mc.goName, ") MsgId32() uint32 { return ", murmur3.Sum32([]byte(mc.goName)), " }")
 	g.P("func (m *", mc.goName, ") Reset() { *m = ", mc.goName, "{} }")
 	if gogoproto.EnabledGoStringer(g.file.FileDescriptorProto, mc.message.DescriptorProto) {
